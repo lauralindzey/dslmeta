751ed2e26280861a8684120512fe274bc6b33d14
jakuba <jakuba@porpentine>
2014-10-23 15:09:10 -0400

    loosened restrictions on install directory and build type.  No longer forced, but defaults are set as before.  Enables some advanced usage cases.

--- cmake/dslmeta-config.cmake
+++ cmake/dslmeta-config.cmake
@@ -1,32 +1,51 @@
 # Set install path for all DSL software.
 SET( DSL_INSTALL_PREFIX /opt/dsl )
 
-# The following breaks with cmake convention.  We force a Debug build
-# and force an installation prefix.  These values cannot be overridden
-# by the user.
+# Set default build options to Debug and add some compiler flags.
+# Users can overwrite the compile flags by setting CMAKE_CXX_FLAGS in
+# their project CMakeLists.txt files. Also increase make's verbosity
+# by default.  The build process will be much more verbose but the
+# actual calls to gcc will be printed.  By placing these SET() calls
+# within an IF block, the default is set but users may override it.  A
+# pure SET call with the FORCE option disallows user modification
+# entirely, whereas without the FORCE option no default is set because
+# ccmake initializes CMAKE_BUILD_TYPE to an empty string upon the call 
+# to PROJECT():
+# http://www.cmake.org/pipermail/cmake/2008-September/023808.html
+# Most variables do not require this wrapper.
+IF(NOT CMAKE_BUILD_TYPE)
+  SET(CMAKE_BUILD_TYPE Debug CACHE STRING
+      "Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel."
+      FORCE)
+ENDIF(NOT CMAKE_BUILD_TYPE)
 
-# Set default build options to Debug and add some compiler flags.  Users
-# can overwrite the compile flags by setting CMAKE_CXX_FLAGS in their
-# project CMakeLists.txt files. Also increase make's verbosity by default.
-# The build process will be much more verbose but the actual calls to gcc 
-# will be printed.  Note: the FORCE option is needed to make these values 
-# show up in the cmake GUI (ccmake) but also disallows changes by the user.
-SET( CMAKE_BUILD_TYPE 
-  Debug CACHE STRING "Build type (Forced)" FORCE
-  )
-set(CMAKE_CXX_FLAGS_DEBUG 
-  "-Wall -Wwrite-strings -ggdb" CACHE STRING "Debug compiler flags (Forced)." FORCE)
+IF(NOT CMAKE_CXX_FLAGS_DEBUG)
+  set(CMAKE_CXX_FLAGS_DEBUG 
+    "-Wall -Wwrite-strings -ggdb" CACHE STRING "Debug compiler flags." FORCE)
+ENDIF(NOT CMAKE_CXX_FLAGS_DEBUG)
+
 set(CMAKE_VERBOSE_MAKEFILE ON)
 
-# Set installation location prefix, used on 'make install'.
-SET(CMAKE_INSTALL_PREFIX
-  ${DSL_INSTALL_PREFIX} CACHE PATH "Install prefix for binaries, headers, etc. (Forced)" FORCE
-  )
+# Set installation location prefix, used on 'make install'.  Generally
+# should not be changed because only the default is placed on the
+# users path.  In this case cmake initializes this variable by default
+# (/usr/local), so the solution is different from CMAKE_BUILD_TYPE:
+# http://stackoverflow.com/questions/16074598/set-installation-prefix-automatically-to-custom-path-if-not-explicitly-specified
+IF(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  SET(CMAKE_INSTALL_PREFIX
+    ${DSL_INSTALL_PREFIX} CACHE PATH 
+    "Install prefix for binaries, headers, etc." FORCE
+    )
+ENDIF(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
 
-# Enforce building from the build subdirectory only.  Annoyingly, cmake cache files will still be created.
+# Enforce building from the build subdirectory only.  Annoyingly,
+# cmake cache files will still be created.  It is not possible to
+# e.g. EXECUTE_PROCESS to remove these files:
+# http://public.kitware.com/Bug/view.php?id=6672
+# so just tell the user to.
 STRING(COMPARE EQUAL "${PROJECT_BINARY_DIR}" "${PROJECT_SOURCE_DIR}/build" INBUILD)
 IF(NOT INBUILD)
-  MESSAGE(FATAL_ERROR "This project enforces and out-of-source build.  Please cd to the 'build' directory to run cmake: 'cmake ../'.")
+  MESSAGE(FATAL_ERROR "This project enforces and out-of-source build.  Please remove CmakeCache.txt and the CMakeFiles directory just created in the current directory, then cd to the 'build' directory to run cmake: 'cmake ../'.")
 ENDIF(NOT INBUILD)
 
 
